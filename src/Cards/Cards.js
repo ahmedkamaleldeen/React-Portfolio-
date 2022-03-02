58addece570fcf2c66ca2be4c60655d70129d49a
ahmedkamaleldeen <ahmedkamaleldeen96@gmail.com>
2022-03-02 21:14:17 +0200

    using if condition in card component

--- src/Cards/Cards.js
+++ src/Cards/Cards.js
@@ -1,21 +1,40 @@
 function ShowCards(props) {
   return (
     <>
-      {props.Projects.map((project) => {
+      {props.Projects.map((project,index) => {
+          if(index%2===0){
         return (
           <div className="col-4 p-5">
             <div className="d-flex justify-content-center align-items-center"
               style={{
-                width: "300px",
+                width: "400px",
                 height: "300px",
                 border: "4px solid black",
-                backgroundColor:"aqua"
+                backgroundColor:"aqua",
               }}
             >
-              <h1>{project}</h1>
+                
+              <h1 style={{borderBottom:"4px solid black",padding:"20px"}}>{project}</h1>
             </div>
           </div>
-        );
+        );}else {
+            return(
+            <div className="col-4 p-5">
+            <div className="d-flex justify-content-center align-items-center"
+              style={{
+                width: "400px",
+                height: "300px",
+                border: "4px solid black",
+                backgroundColor:"red",
+              }}
+            >
+                
+              <h1 style={{borderBottom:"4px solid black",padding:"20px"}}>{project}</h1>
+            </div>
+          </div>
+            );
+
+        }
       })}
     </>
   );
